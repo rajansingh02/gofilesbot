88cc6f37c275ee0638ade8759f2903a49966df20
Rajan Singh <84074133+rajansingh02@users.noreply.github.com>
2022-08-03 13:24:05 +0530

    Update pm_query.py

--- plugins/pm_query.py
+++ plugins/pm_query.py
@@ -71,7 +71,7 @@ async def bot_pm(client: Bot, message: Message):
                                 chat_id=message.chat.id,
                                 from_chat_id=messages.chat.id,
                                 message_id=messages.message_id,
-                                caption=Config.GROUP_U_NAME+{tiltle}+Presets.CAPTION_TEXT_DOC.format(media_name,
+                                caption=Config.GROUP_U_NAME+Presets.CAPTION_TEXT_DOC.format(media_name,
                                                                                             media_format, file_size)
                             )
                         except FloodWait as e:
@@ -91,7 +91,7 @@ async def bot_pm(client: Bot, message: Message):
                                 chat_id=message.chat.id,
                                 from_chat_id=messages.chat.id,
                                 message_id=messages.message_id,
-                                caption=Config.GROUP_U_NAME+{tiltle}+Presets.CAPTION_TEXT_VID.format(media_name, file_size)
+                                caption=Config.GROUP_U_NAME+Presets.CAPTION_TEXT_VID.format(media_name, file_size)
                             )
                         except FloodWait as e:
                             time.sleep(e.x)
